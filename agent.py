39d5c7a3741ce8be755353c565f9c4ddbdf322a4
pahan <ktoto3111@gmail.com>
2026-03-13 14:47:42 +0300

    feat: implement CLI agent for LLM (Task 1)

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ import json
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
-#dummi things
+#;asldf
 def main():
     # Загружаем переменные из .env.agent.secret
     load_dotenv('.env.agent.secret')
